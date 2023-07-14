cf444215df5b8a5200468d0b0fdacbf485edd1a0
Jatin <githubgit.repositories@gmail.com>
2023-07-14 22:18:04 +0530

    pop and shift added to linked list

--- DataStructures/SinglyLinkedList.js
+++ DataStructures/SinglyLinkedList.js
@@ -6,7 +6,7 @@ class Node {
 }
 
 class SingleLinkedList {
-  constructor(head, tail, length) {
+  constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
@@ -16,15 +16,45 @@ class SingleLinkedList {
     var node = new Node(value);
     if (!this.head) this.head = this.tail = node;
     else {
-      this.head.next = node;
+      this.tail.next = node;
       this.tail = node;
     }
     this.length++;
     return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    while (current.next !== this.tail && this.head !== this.tail)
+      current = current.next;
+    this.tail = current;
+    const popped = this.tail.next === null ? this.tail : this.tail.next;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return popped;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    const shifted = this.head;
+    if (this.head.next) this.head = this.head.next;
+    else this.head = this.tail = null;
+    this.length--;
+    return shifted;
   }
 }
 
 const list = new SingleLinkedList();
 list.push("value 1");
 list.push("value 2");
+list.push("value 3");
+console.log(list.pop());
+console.log(list.pop());
+console.log(list.shift());
+// console.log(list.pop());
 console.log(list);
