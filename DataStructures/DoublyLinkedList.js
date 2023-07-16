77e22e414aa756ee523b8fc23c5b9958fc1cedc2
Jatin <githubgit.repositories@gmail.com>
2023-07-16 22:32:42 +0530

    unshift and push added to doubly linked list

--- DataStructures/DoublyLinkedList.js
+++ DataStructures/DoublyLinkedList.js
@@ -50,6 +50,38 @@ class DoublyLinkedList {
     this.length--;
     return oldHead;
   }
+
+  unshift(value) {
+    const node = new Node(value);
+    if (!head) this.head = this.tail = node;
+    else {
+      node.next = this.head;
+      this.head.prev = node;
+      this.head = node;
+    }
+    this.length++;
+  }
+
+  get(index) {
+    if (index < 0 || index > this.length - 1) return undefined;
+    if (index <= this.length / 2) {
+      let count = 0;
+      let current = this.head;
+      while (count !== index) {
+        count++;
+        current = current.next;
+      }
+      return current;
+    } else {
+      let count = this.length - 1;
+      let current = this.tail;
+      while (count !== index) {
+        count--;
+        current = current.prev;
+      }
+      return current;
+    }
+  }
 }
 
 const list = new DoublyLinkedList();
@@ -57,15 +89,17 @@ list.push(1);
 list.push(2);
 list.push(3);
 list.push(4);
-list.pop();
-console.log(list.shift());
+// list.pop();
+// console.log(list.shift());
+console.log(list.get(1));
+console.log(list.get(3));
 console.group(
-  "\u001b[" + 32 + "m" + "----------------NEW LINE-----------" + "\u001b[0m"
+  "\u001b[" + 31 + "m" + "----------------NEW LINE-----------" + "\u001b[0m"
 );
-// for (let i = 0; i < list.length; i++) {
-//   console.group(
-//     list.get(i),
-//     "\u001b[" + 31 + "m" + "List Item " + i + "\u001b[0m"
-//   );
-// }
-console.log(list);
+for (let i = 0; i < list.length; i++) {
+  console.group(
+    list.get(i),
+    "\u001b[" + 32 + "m" + "List Item " + i + "\u001b[0m"
+  );
+}
+// console.log(list);
