5930341ee22de1d79dc61235ba77b2e8d7240839
Jatin <githubgit.repositories@gmail.com>
2023-07-17 23:37:37 +0530

    blueprint of binarySearchTree added

---
+++ DataStructures/BinarySearchTree.js
@@ -0,0 +1,20 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    let node = new Node(value);
+    let current = this.root;
+    let found;
+    while (!found) {}
+  }
+}
