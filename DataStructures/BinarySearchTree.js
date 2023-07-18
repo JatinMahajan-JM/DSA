c4cdb5e032cbe3f9c6e42c570e5dc5940112a8db
Jatin <githubgit.repositories@gmail.com>
2023-07-18 22:32:39 +0530

    added find in binarySearchTree

--- DataStructures/BinarySearchTree.js
+++ DataStructures/BinarySearchTree.js
@@ -33,7 +33,37 @@ class BinarySearchTree {
     }
   }
 
-  // find()
+  // find(value) {
+  //   if (!this.root) return false;
+  //   let current = this.root;
+  //   while (true) {
+  //     if (current.value === value) return current;
+  //     if (value < current.value) {
+  //       if (current.left) current = current.left;
+  //       else return false;
+  //     } else {
+  //       if (current.right) current = current.right;
+  //       else return false;
+  //     }
+  //   }
+  // }
+
+  find(value) {
+    if (!this.root) return false;
+    let current = this.root;
+    let found = false;
+    while (!found && current) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return false;
+    return current;
+  }
 }
 
 const bst = new BinarySearchTree();
@@ -42,7 +72,9 @@ bst.insert(2);
 bst.insert(0);
 bst.insert(-4);
 bst.insert(0);
+bst.insert(4);
 console.log(bst);
 console.log(bst.root.left.left);
+console.log(bst.find(0));
 
 // } else if (node.value <= current.value) {
