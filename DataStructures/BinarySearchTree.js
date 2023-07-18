dc4462e92d34dc41ad0c826d8b9aa870c966066a
Jatin <githubgit.repositories@gmail.com>
2023-07-18 22:15:58 +0530

    insert added in binarySearchTree

--- DataStructures/BinarySearchTree.js
+++ DataStructures/BinarySearchTree.js
@@ -13,8 +13,36 @@ class BinarySearchTree {
 
   insert(value) {
     let node = new Node(value);
+    if (!this.root) return (this.root = node);
     let current = this.root;
-    let found;
-    while (!found) {}
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value > current.value) {
+        if (current.right) current = current.right;
+        else {
+          current.right = node;
+          return this;
+        }
+      } else {
+        if (current.left) current = current.left;
+        else {
+          current.left = node;
+          return this;
+        }
+      }
+    }
   }
+
+  // find()
 }
+
+const bst = new BinarySearchTree();
+bst.insert(1);
+bst.insert(2);
+bst.insert(0);
+bst.insert(-4);
+bst.insert(0);
+console.log(bst);
+console.log(bst.root.left.left);
+
+// } else if (node.value <= current.value) {
