ae43e110878c1af747221eecd28ec6ee39c7b171
Jatin <githubgit.repositories@gmail.com>
2023-07-18 21:43:47 +0530

    added an experimental console file

---
+++ Console.js
@@ -0,0 +1,12 @@
+const data = [
+  { name: "John", age: 25, goal: "consoles" },
+  { name: "Jane", age: 30 },
+];
+console.table(data);
+let x = 1;
+console.debug("The value of x is:", x);
+console.info("This function calculates the sum of two numbers.");
+console.warn("This feature is deprecated. Please update your code.");
+console.error("An error occurred!");
+
+console.log(JSON.stringify(data[0], null, 1));
