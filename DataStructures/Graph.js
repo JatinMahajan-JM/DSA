3fc8d6a23e698a3f19dfe58974f7e6398d8e17bf
Jatin <githubgit.repositories@gmail.com>
2023-07-24 22:30:21 +0530

    added removeVertex method

--- DataStructures/Graph.js
+++ DataStructures/Graph.js
@@ -21,6 +21,14 @@ class Graph {
       (vertex) => vertex !== v1
     );
   }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const currVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(currVertex, vertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
 }
 
 const graph = new Graph();
@@ -32,5 +40,6 @@ graph.addEdge("v1", "v4");
 graph.addEdge("v2", "v3");
 graph.addEdge("v2", "v1");
 graph.addEdge("v4", "v3");
-graph.removeEdge("v1", "v2");
+// graph.removeEdge("v1", "v2");
+graph.removeVertex("v1");
 console.log(graph);
