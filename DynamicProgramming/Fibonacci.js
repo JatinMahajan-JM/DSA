081f0cbb172d9be4117a17cf3b385e0a93c3dbda
Jatin <githubgit.repositories@gmail.com>
2023-07-28 00:20:56 +0530

    tabulation or bottom up approach added for fibonacci function

--- DynamicProgramming/Fibonacci.js
+++ DynamicProgramming/Fibonacci.js
@@ -7,3 +7,13 @@ function Fibonacci(num, memo = []) {
 }
 
 console.log(Fibonacci(16));
+
+//bottom up
+function fibonacci_tabulation(n) {
+  if (n <= 2) return 1;
+  let fibonacci = [0, 0, 1];
+  for (let i = 3; i <= n; i++) {
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+  }
+  return fibonacci[n];
+}
