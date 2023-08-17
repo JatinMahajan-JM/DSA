883373af4d198f5a8852cd1ebb7ff520b69ee8cb
Jatin <githubgit.repositories@gmail.com>
2023-08-17 23:53:49 +0530

    bug fix in old method and added new method

--- FrequencyCounter/Anagram.js
+++ FrequencyCounter/Anagram.js
@@ -3,8 +3,10 @@
 function checkAnagram(str1, str2) {
   const obj1 = {};
   for (let item of str1) {
-    obj1[item] = obj1[item] ?? 0 + 1;
+    obj1[item] = (obj1[item] ?? 0) + 1;
   }
+
+  console.log(obj1)
 
   for (let item of str2) {
     if (!obj1[item]) return false;
@@ -14,4 +16,11 @@ function checkAnagram(str1, str2) {
   return true;
 }
 
+function checkAnagram2(str1, str2) {
+  return str1.split("").sort().join("") === str2.split("").sort().join("")
+}
+
 console.log(checkAnagram("iceman", "cinema"));
+console.log(checkAnagram("hhhhu", "hhhuh"));
+console.log(checkAnagram2("hhhhu", "hhhuh"));
+
